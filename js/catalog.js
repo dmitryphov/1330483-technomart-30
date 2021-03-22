c4564528f1e7518e23bbd8c395f34074223ea05c
dmitryphov <dmitriyphov@mail.ru>
2021-03-22 16:36:06 +0500

    интерактивные элементы

---
+++ js/catalog.js
@@ -0,0 +1,21 @@
+const buyButtons = document.querySelectorAll(".catalog-btn_buy");
+const addToCartModal = document.querySelector(".popup-cart");
+const addToCartClose = addToCartModal.querySelector(".modal-close");
+const modalContinue = addToCartModal.querySelector(".continue-button");
+
+buyButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    addToCartModal.classList.add("modal-show")
+  })
+})
+
+addToCartClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  addToCartModal.classList.remove("modal-show");
+});
+
+modalContinue.addEventListener("click", function (e) {
+  e.preventDefault();
+  addToCartModal.classList.remove("modal-show");
+});
